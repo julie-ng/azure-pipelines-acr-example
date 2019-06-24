6cc8996df06e579cc6ed1f501b65bf1a337b205b
Julie Ng <me@julie.io>
2019-06-24 19:26:52 +0200

    add healthcheck demo

--- app/app.js
+++ app/app.js
@@ -12,6 +12,25 @@ app.get('/', (req, res) => {
 	res.send('Hello World! V2')
 })
 
+app.get('/health', (req, res) => {
+	console.log(process.env)
+	let body = {
+		status: 'pass',
+		version: '1',
+		description: 'An Azure App Service Demo using Azure DevOps and Azure Container Registry (ACR)',
+		details: {
+			uptime: {
+				componentType: 'system',
+				observedValue: process.uptime(),
+				observedUnit: 's',
+				status: 'pass',
+				time: new Date()
+			}
+		}
+	}
+	res.json(body)
+})
+
 app.use((req, res, next) => {
   res.status(404).send('Oops - page not found.')
 })
